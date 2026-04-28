586f24c1f97e1116c1a1cea54423a1415dbfc902
unitweakm-stack <unitweakm@gmail.com>
2026-04-28 18:43:35 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,4 @@
 import os
 
-# Railway Environment Variables dan o'qiydi
-# Agar u yerda topilmasa, default qiymatlarni oladi
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
-CHANNEL_ID = os.environ.get("CHANNEL_ID", "@your_channel_username")
-
-# ADMIN_ID doim raqam (int) bo'lishi kerak
-try:
-    ADMIN_ID = int(os.environ.get("ADMIN_ID", 123456789))
-except (ValueError, TypeError):
-    ADMIN_ID = 0 
+TOKEN = os.getenv("BOT_TOKEN")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
